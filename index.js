4a7a50c9b89748d79be03d5e8435531785370bdb
ningl <dummyedu@gmail.com>
2017-10-18 10:19:59 +0800

    Add simple implementation

---
+++ index.js
@@ -0,0 +1,42 @@
+const shelljs = require('shelljs');
+const _ = require('lodash');
+const program = require('commander');
+
+program
+  .version('1.0.0')
+  .option('-p, --port <string>', 'server port')
+  .parse(process.argv);
+
+const port = parseInt(program.port, 10) || 4010;
+
+const getUrl = url => {
+	return new Promise((resolve, reject) => {
+		shelljs.exec(`youtube-dl -g ${url}`, function(code, stdout, stderr) {
+			if (stdout != null && stdout.length > 0) {
+				resolve(stdout.split('\n')[0]);
+			} else {
+				reject();
+			}
+		});
+	})
+	
+}
+
+// getUrl('https://www.youtube.com/watch?v=qRNB8CV3_LU')
+// 	.then(console.log)
+
+var express = require('express');
+var app = express();
+
+app.get('/youtube/:watchId', function (req, res) {
+	const watchId = req.params.watchId;
+	getUrl(`https://www.youtube.com/watch?v=${watchId}`)
+		.then(downloadUrl => {
+			res.send(downloadUrl);
+		});
+});
+
+app.listen(port, function () {
+  console.log(`video url parse app listening on port ${port}!`);
+});
+
